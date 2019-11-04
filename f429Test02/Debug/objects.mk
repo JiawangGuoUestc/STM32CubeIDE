f616f5a71c13cef440507db7dcddebded26d6edd
JiawangGuoUestc <guo_jia_wang@163.com>
2019-11-04 23:16:29 +0800

    添加成功stm32DSP库

--- f429Test02/Debug/objects.mk
+++ f429Test02/Debug/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS :=
+USER_OBJS := E:/code/STM32CubeIDE/f429Test02/Lib/GCC/libarm_cortexM4lf_math.a
 
 LIBS :=
 
--- f429Test02/Src/main.c
+++ f429Test02/Src/main.c
@@ -17,91 +17,88 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
-
+#define ARM_MATH_CM4
+#define __FPU_PRESENT = 1
+#define ARM_MATH_MATRIX_CHECK
+#define ARM_MATH_ROUNDING
 /* Includes ------------------------------------------------------------------*/
+
 #include "main.h"
+#include "arm_math.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
 
-/* USER CODE END Includes */
+#define MAX_BLOCKSIZE   32
+#define DELTA           (0.0001f)
 
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
+
 void Delay(__IO uint32_t nCount);
 
 void Delay(__IO uint32_t nCount)
 {
   while(nCount--){}
 }
-/* USER CODE END PTD */
 
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
-
-/* USER CODE BEGIN PV */
-
-/* USER CODE END PV */
-
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
-
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
+const float32_t testInput_f32[MAX_BLOCKSIZE] =
 {
-  /* USER CODE BEGIN 1 */
+  -1.244916875853235400,  -4.793533929171324800,   0.360705030233248850,   0.827929644170887320,  -3.299532218312426900,   3.427441903227623800,   3.422401784294607700,  -0.108308165334010680,
+   0.941943896490312180,   0.502609575000365850,  -0.537345278736373500,   2.088817392965764500,  -1.693168684143455700,   6.283185307179590700,  -0.392545884746175080,   0.327893095115825040,
+   3.070147440456292300,   0.170611405884662230,  -0.275275082396073010,  -2.395492805446796300,   0.847311163536506600,  -3.845517018083148800,   2.055818378415868300,   4.672594161978930800,
+  -1.990923030266425800,   2.469305197656249500,   3.609002606064021000,  -4.586736582331667500,  -4.147080139136136300,   1.643756718868359500,  -1.150866392366494800,   1.985805026477433800
 
-  /* USER CODE END 1 */
-  
 
-  /* MCU Configuration--------------------------------------------------------*/
+};
+const float32_t testRefOutput_f32 = 1.000000000;
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+uint32_t blockSize = 32;
+float32_t  testOutput;
+float32_t  cosOutput;
+float32_t  sinOutput;
+float32_t  cosSquareOutput;
+float32_t  sinSquareOutput;
+arm_status status;
+
+
+int32_t main(void)
+{
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+  float32_t diff;
+  uint32_t i;
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+  for(i=0; i< blockSize; i++)
+    {
+      cosOutput = arm_cos_f32(testInput_f32[i]);
+      float32_t cosOutput_test = cosOutput;
+      sinOutput = arm_sin_f32(testInput_f32[i]);
+      float32_t sinOutput_test = sinOutput;
+
+      arm_mult_f32(&cosOutput, &cosOutput, &cosSquareOutput, 1);
+      arm_mult_f32(&sinOutput, &sinOutput, &sinSquareOutput, 1);
+
+      arm_add_f32(&cosSquareOutput, &sinSquareOutput, &testOutput, 1);
+
+      /* absolute value of difference between ref and test */
+      diff = fabsf(testRefOutput_f32 - testOutput);
+
+      /* Comparison of sin_cos value with reference */
+      if(diff > DELTA)
+      {
+         status = ARM_MATH_TEST_FAILURE;
+      }
+      if( status == ARM_MATH_TEST_FAILURE)
+      {
+         while(1);
+      }
+    }
+
   while (1)
   {
-    /* USER CODE END WHILE */
+
 	  HAL_GPIO_WritePin(GPIOB,GPIO_PIN_0,GPIO_PIN_SET);	//PB1置1
 	  	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_RESET);	//PB0置1
 	  	//Delay(0x7FFFFF);
@@ -110,9 +107,9 @@ int main(void)
 	  	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_SET);	//PB0置0
 	  	//Delay(0x7FFFFF);
 	  	HAL_Delay(500);
-    /* USER CODE BEGIN 3 */
+
   }
-  /* USER CODE END 3 */
+
 }
 
 /**
